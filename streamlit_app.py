0c7efd13c2d5a7c548fa6bdc94ffdae0a8ebf096
oyatsugohan <masa11.12gannboruto@icloud.com>
2025-09-24 09:19:57 +0000

    最初にどんなご用件か聞く　修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ st.title('フィッシング詐欺対策アプリ')
 user_name = st.text_input ('あなたの名前を教えてください')
 if st.button('挨拶する'):
     if user_name:  # 名前が入力されているかチェック
-        st.success(f' こんにちは、{user_name}さん! 今回はどのようなご用件でしょうか')  # メッセージをハイライト
+        st.success(f' こんにちは、{user_name}さん。 今回はどのようなご用件でしょうか')  # メッセージをハイライト
     else:
         st.error('名前を入力してください。')  # エラーメッセージを表示
